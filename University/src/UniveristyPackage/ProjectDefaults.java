5ff78c54b53a326ac9971b589711358be0932d59
Kuba Prus <prusik7@gmail.com>
2017-11-24 11:26:57 +0100

    Deleted unnecessery atriubtes

--- University/src/UniveristyPackage/ProjectDefaults.java
+++ University/src/UniveristyPackage/ProjectDefaults.java
@@ -5,8 +5,5 @@ import java.time.format.DateTimeFormatter;
 
 class ProjectDefaults {
     public static final String createdBy = "Admin";
-    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd");
-    private final LocalDate localDate = LocalDate.now();
-    String date = localDate.toString();
     static final LocalDate createdDate = LocalDate.now();
 }
